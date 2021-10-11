3f15638d7b34300c80ebad1f3df488153928674c
Yoshimura Naoya <yoshimura708x@gmail.com>
2021-10-12 01:18:08 +0900

    fix: update commands #1

--- control/tests/test_commands.py
+++ control/tests/test_commands.py
@@ -48,5 +48,198 @@ def test_GetDeviceTime__encode__01():
     assert msg_out == msg_exp
     
     
+@pytest.mark.parametrize("response,ts_exp", (
+    (
+        bytes([0x9A, 0x92, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x00, 0x00, 0x8b]),
+        datetime.datetime(2001, 1, 1, 1, 1, 1, 0),
+    ),
+    (
+        bytes([0x9A, 0x92, 0x15, 0x0A, 0x0B, 0x01, 0x01, 0x01, 0x01, 0x01, 0x8b]),
+        datetime.datetime(2021, 10, 11, 1, 1, 1, 257000),
+    ),
+))
+def test_GetDeviceTime__decode__01(response,ts_exp):
+    print("response", response)
+
+    cobj = cmd.GetDeviceTime()
+    ts_out = cobj.decode(response)
+    print("ts[Expected]:", ts_exp)
+    print("ts[Outputs ]:", ts_out)
+
+    assert ts_out == ts_exp
+    
+
+@pytest.mark.parametrize("interval,send_interval,recording_interval,msg_exp", (
+    (
+        33, 
+        1,
+        0,
+        bytes([0x9A, 0x16, 0x21, 0x01, 0x00, 0xac]),
+    ),
+    (
+        33, 
+        255,
+        1,
+        bytes([0x9A, 0x16, 0x21, 0xFF, 0x01, 0x53]),
+    ),
+))
+def test_SetAgsMethod__01(interval,send_interval,recording_interval,msg_exp):
+    print("Interval [ms]:", interval)
+    print("Send Interval [ms]:", send_interval)
+    print("Recording Interval [ms]:", recording_interval)
+
+    cobj = cmd.SetAgsMethod()
+    msg_out = cobj.encode(interval,send_interval,recording_interval)
+    print("msg[Expected]:", list(msg_exp))
+    print("msg[Outputs ]:", list(msg_out))
+
+    # np.testing.assert_array_equal(msg_out, msg_exp)
+    assert msg_out == msg_exp
 
 
+
+
+@pytest.mark.parametrize("response, interval, send_freq, record_freq", (
+    (
+        bytes([0x9A, 0x97, 0x21, 0x01, 0x00, 0x00]),
+        33, 
+        1,
+        0,
+    ),
+    (
+        bytes([0x9A, 0x97, 0x21, 0xFF, 0x01, 0x00]),
+        33, 
+        255,
+        1,
+    ),
+))
+def test_GetAgsMethod__01(response, interval, send_freq, record_freq):
+    cobj = cmd.GetAgsMethod()
+    data = cobj.decode(response)
+    print("data:", data)
+
+    assert data.get("interval") == interval
+    assert data.get("send_freq") == send_freq
+    assert data.get("record_freq") == record_freq
+
+
+@pytest.mark.parametrize("interval,send_interval,recording_interval,msg_exp", (
+    (
+        0, 
+        0,
+        0,
+        bytes([0x9A, 0x18, 0x00, 0x00, 0x00, 0x82]),
+    ),
+))
+def test_SetGeoMagneticMethod__01(interval,send_interval,recording_interval,msg_exp):
+    print("Interval [ms]:", interval)
+    print("Send Interval [ms]:", send_interval)
+    print("Recording Interval [ms]:", recording_interval)
+
+    cobj = cmd.SetGeoMagneticMethod()
+    msg_out = cobj.encode(interval,send_interval,recording_interval)
+    print("msg[Expected]:", list(msg_exp))
+    print("msg[Outputs ]:", list(msg_out))
+
+    # np.testing.assert_array_equal(msg_out, msg_exp)
+    assert msg_out == msg_exp
+
+
+
+@pytest.mark.parametrize("response, interval, send_freq, record_freq", (
+    (
+        bytes([0x9A, 0x99, 0x00, 0x00, 0x00, 0x00]),
+        0, 
+        0,
+        0,
+    ),
+))
+def test_GetGeoMagneticMethod__01(response, interval, send_freq, record_freq):
+    cobj = cmd.GetGeoMagneticMethod()
+    data = cobj.decode(response)
+    print("data:", data)
+
+    assert data.get("interval") == interval
+    assert data.get("send_freq") == send_freq
+    assert data.get("record_freq") == record_freq
+
+
+
+@pytest.mark.parametrize("interval,send_interval,recording_interval,msg_exp", (
+    (
+        0, 
+        0,
+        0,
+        bytes([0x9A, 0x1A, 0x00, 0x00, 0x00, 0x80]),
+    ),
+))
+def test_SetPresMethod__01(interval,send_interval,recording_interval,msg_exp):
+    print("Interval [ms]:", interval)
+    print("Send Interval [ms]:", send_interval)
+    print("Recording Interval [ms]:", recording_interval)
+
+    cobj = cmd.SetPresMethod()
+    msg_out = cobj.encode(interval,send_interval,recording_interval)
+    print("msg[Expected]:", list(msg_exp))
+    print("msg[Outputs ]:", list(msg_out))
+
+    # np.testing.assert_array_equal(msg_out, msg_exp)
+    assert msg_out == msg_exp
+
+
+
+@pytest.mark.parametrize("response, interval, send_freq, record_freq", (
+    (
+        bytes([0x9A, 0x9B, 0x00, 0x00, 0x00, 0x00]),
+        0, 
+        0,
+        0,
+    ),
+))
+def test_GetPresMethod__01(response, interval, send_freq, record_freq):
+    cobj = cmd.GetPresMethod()
+    data = cobj.decode(response)
+    print("data:", data)
+
+    assert data.get("interval") == interval
+    assert data.get("send_freq") == send_freq
+    assert data.get("record_freq") == record_freq
+
+
+
+
+@pytest.mark.parametrize("send,record,msg_exp", (
+    (
+        0,
+        0,
+        bytes([0x9A, 0x1C, 0x00, 0x00, 0x86]),
+    ),
+))
+def test_SetBattMethod__01(send,record,msg_exp):
+    print("Send [0/1]:", send)
+    print("Record [0/1]:", record)
+
+    cobj = cmd.SetBattMethod()
+    msg_out = cobj.encode(send, record)
+    print("msg[Expected]:", list(msg_exp))
+    print("msg[Outputs ]:", list(msg_out))
+
+    # np.testing.assert_array_equal(msg_out, msg_exp)
+    assert msg_out == msg_exp
+
+
+
+@pytest.mark.parametrize("response, send, record", (
+    (
+        bytes([0x9A, 0x9D, 0x00, 0x00, 0x00]),
+        0, 
+        0,
+    ),
+))
+def test_GetBattMethod__01(response, send, record):
+    cobj = cmd.GetBattMethod()
+    data = cobj.decode(response)
+    print("data:", data)
+
+    assert data.get("send") == send
+    assert data.get("record") == record
--- control/tsndctl/commands.py
+++ control/tsndctl/commands.py
@@ -19,7 +19,8 @@ def add_bcc(cmd: List[int]):
 class CmdTemplate(object):
     """ Base class for TSDN command  serial communication.
     """
-    cmd_code: bytes = None 
+    cmd_code: int = None 
+    response_code: int = None
     pyload: bytes = None
     
     def __init__(self):
@@ -34,6 +35,7 @@ class CmdTemplate(object):
 
 class SetDeviceTime(CmdTemplate):
     cmd_code = 0x11
+    response_code = 0x8F
 
     def encode(self, ts: datetime.datetime):
         ms = int(ts.microsecond // 1e3)
@@ -52,8 +54,14 @@ class SetDeviceTime(CmdTemplate):
         cmd = add_bcc(cmd)
         return bytes(cmd)
 
+    def decode(self, response):
+        assert response[1] == self.response_code
+        data = response[2]
+        return data
+
 class GetDeviceTime(CmdTemplate):
     cmd_code = 0x12
+    response_code = 0x92
 
     def encode(self):
         cmd = [CMD_HEADER, self.cmd_code, 0x00]
@@ -61,7 +69,244 @@ class GetDeviceTime(CmdTemplate):
 
         return bytes(cmd)
 
-    def decode(self, response):
-        raise NotImplementedError()
+    def decode(self, response: bytes) -> datetime.datetime:
+        # raise NotImplementedError()
+        assert response[1] == self.response_code
+        
+        # -- Decode --
+        # # year
+        # data = [(response[2] + 2000)]
+        # # month, day, hour, minute, second 
+        # data += list(response[3:8])
+        # # milli second,
+        # data.append(int.from_bytes(response[8:10], 'little'))
+        # ts = datetime.datetime(*data)
+        ts = datetime.datetime(
+            (response[2] + 2000), # year
+            response[3], # month
+            response[4], # day
+            hour=response[5],
+            minute=response[6],
+            second=response[7],
+            microsecond=(int.from_bytes(response[8:10], "little") * 1000),
+        )
+        return ts
 
-    
+
+class SetAgsMethod(CmdTemplate):
+    """加速/角速度計測設定取得
+    """
+    cmd_code = 0x16
+    response_code = 0x8F
+
+    def encode(self, interval, send_freq, record_freq):
+        """
+        Args:
+            interval (int): 計測の実施有無及び計測周期を設定.
+                0:計測 OFF, 1~255: 計測周期 (1ms 単位指定)
+            send_freq (int): 計測データ送信の実施有無及び送信時の平均回数を設定
+                0:送信しない, 1~255: 平均回数
+            record_freq (int): 計測データ記録の実施有無及び記録時の平均回数を設定
+                0:記録しない, 1~255: 平均回数
+        """
+        assert interval >= 0 and interval < 256
+        assert send_freq >= 0 and send_freq < 256
+        assert record_freq >= 0 and record_freq < 256
+
+        cmd = [
+            CMD_HEADER,
+            self.cmd_code,
+            interval,
+            send_freq,
+            record_freq,
+        ]
+        cmd = add_bcc(cmd)
+        return bytes(cmd)
+
+    def decode(self, response):
+        assert response[1] == self.response_code
+        data = response[2]
+        return data
+
+
+class GetAgsMethod(CmdTemplate):
+    """加速/角速度計測設定
+    """
+    cmd_code = 0x17
+    response_code = 0x97
+
+    def encode(self):
+        cmd = [CMD_HEADER, self.cmd_code, 0x00]
+        cmd = add_bcc(cmd)
+        return bytes(cmd)
+
+    def decode(self, response):
+        assert response[1] == self.response_code
+        data = {
+            "interval": response[2],
+            "send_freq": response[3],
+            "record_freq": response[4],
+        }
+        return data
+
+class SetGeoMagneticMethod(CmdTemplate):
+    """地磁気計測設定
+    """
+    cmd_code = 0x18
+    response_code = 0x8F
+
+    def encode(self, interval, send_freq, record_freq):
+        """
+        Args:
+            interval (int): 計測の実施有無及び計測周期を設定.
+                0:計測 OFF, 1~255: 計測周期 (1ms 単位指定)
+            send_freq (int): 計測データ送信の実施有無及び送信時の平均回数を設定
+                0:送信しない, 1~255: 平均回数
+            record_freq (int): 計測データ記録の実施有無及び記録時の平均回数を設定
+                0:記録しない, 1~255: 平均回数
+        """
+        assert interval >= 0 and interval < 256
+        assert send_freq >= 0 and send_freq < 256
+        assert record_freq >= 0 and record_freq < 256
+
+        cmd = [
+            CMD_HEADER,
+            self.cmd_code,
+            interval,
+            send_freq,
+            record_freq,
+        ]
+        cmd = add_bcc(cmd)
+        return bytes(cmd)
+
+    def decode(self, response):
+        assert response[1] == self.response_code
+        data = response[2]
+        return data
+
+class GetGeoMagneticMethod(CmdTemplate):
+    """地磁気計測設定取得
+    """
+    cmd_code = 0x19
+    response_code = 0x99
+
+    def encode(self):
+        cmd = [CMD_HEADER, self.cmd_code, 0x00]
+        cmd = add_bcc(cmd)
+        return bytes(cmd)
+
+    def decode(self, response):
+        assert response[1] == self.response_code
+        data = {
+            "interval": response[2],
+            "send_freq": response[3],
+            "record_freq": response[4],
+        }
+        return data
+
+
+class SetPresMethod(CmdTemplate):
+    """気圧計測設定
+    """
+    cmd_code = 0x1A
+    response_code = 0x8F
+
+    def encode(self, interval, send_freq, record_freq):
+        """
+        Args:
+            interval (int): 計測の実施有無及び計測周期を設定.
+                0:計測 OFF, 1~255: 計測周期 (1ms 単位指定)
+            send_freq (int): 計測データ送信の実施有無及び送信時の平均回数を設定
+                0:送信しない, 1~255: 平均回数
+            record_freq (int): 計測データ記録の実施有無及び記録時の平均回数を設定
+                0:記録しない, 1~255: 平均回数
+        """
+        assert interval >= 0 and interval < 256
+        assert send_freq >= 0 and send_freq < 256
+        assert record_freq >= 0 and record_freq < 256
+
+        cmd = [
+            CMD_HEADER,
+            self.cmd_code,
+            interval,
+            send_freq,
+            record_freq,
+        ]
+        cmd = add_bcc(cmd)
+        return bytes(cmd)
+
+    def decode(self, response):
+        assert response[1] == self.response_code
+        data = response[2]
+        return data
+
+class GetPresMethod(CmdTemplate):
+    """地磁気計測設定取得
+    """
+    cmd_code = 0x1B
+    response_code = 0x9B
+
+    def encode(self):
+        cmd = [CMD_HEADER, self.cmd_code, 0x00]
+        cmd = add_bcc(cmd)
+        return bytes(cmd)
+
+    def decode(self, response):
+        assert response[1] == self.response_code
+        data = {
+            "interval": response[2],
+            "send_freq": response[3],
+            "record_freq": response[4],
+        }
+        return data
+
+
+class SetBattMethod(CmdTemplate):
+    """バッテリ電圧計測設定
+    """
+    cmd_code = 0x1C
+    response_code = 0x8F
+
+    def encode(self, send, record):
+        """
+        Args:
+            is_send (int): 計測データ送信の実施有無を設定
+                0:送信しない, 1:送信する.
+            is_record (int): 計測データ記録の実施有無を選択
+                0:記録しない、1:記録する
+        """
+        assert send in [0, 1]
+        assert record in [0, 1]
+
+        cmd = [
+            CMD_HEADER,
+            self.cmd_code,
+            send,
+            record,
+        ]
+        cmd = add_bcc(cmd)
+        return bytes(cmd)
+
+    def decode(self, response):
+        assert response[1] == self.response_code
+        data = response[2]
+        return data
+
+class GetBattMethod(CmdTemplate):
+    """バッテリ電圧計測設定取得
+    """
+    cmd_code = 0x1D
+    response_code = 0x9D
+
+    def encode(self):
+        cmd = [CMD_HEADER, self.cmd_code, 0x00]
+        cmd = add_bcc(cmd)
+        return bytes(cmd)
+
+    def decode(self, response):
+        assert response[1] == self.response_code
+        data = {
+            "send": response[2],
+            "record": response[3],
+        }
+        return data
